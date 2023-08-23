2781a25f7dab7fdd6f33668708a30936b8cce389
Victoria <victorialors@hotmail.com>
2023-08-22 23:48:53 -0500

    Create capitalize function to make player inputs case-insensitive

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    selection = Math.floor(Math.random() * 3)
+    let selection = Math.floor(Math.random() * 3)
     if (selection == 0) {
         selection = "Paper"
     }
@@ -12,8 +12,25 @@ function getComputerChoice() {
     return selection
 }
 
-temp = getComputerChoice()
+// function playRound(playerSelection, computerSelection) {
+//     computerSelection = getComputerChoice()
+//     playerSelection = 
+
+// }
+
+function capitalize(word) {
+    let firstLetter = word.charAt(0)
+    firstLetter = firstLetter.toUpperCase()
+    let remainingLetters = word.substring(1)
+    remainingLetters = remainingLetters.toLowerCase()
+    return firstLetter + remainingLetters
+}
+
+
+// Test your outputs below to ensure correct values
+temp = capitalize("PAPEr")
 console.log(temp)
+
 
 
 
