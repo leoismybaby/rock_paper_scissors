242077e1115cd40d5a644abdfa2440a0ee3163b7
Victoria <victorialors@hotmail.com>
2023-08-23 00:22:22 -0500

    Create function to play a round of rock paper scissors

--- script.js
+++ script.js
@@ -12,11 +12,26 @@ function getComputerChoice() {
     return selection
 }
 
-// function playRound(playerSelection, computerSelection) {
-//     computerSelection = getComputerChoice()
-//     playerSelection = 
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice()
+    playerSelection = capitalize(playerSelection)
 
-// }
+    // if player ties:
+    if (playerSelection == computerSelection) {
+        return "It's a tie!"
+    }
+    // If player loses:
+    else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock") {
+        let outcome = "Lose"
+        return `You ${outcome}! ${computerSelection} beats ${playerSelection}`
+    } 
+    // if player wins:
+    else {
+        let outcome = "Win"
+        return `You ${outcome}! ${playerSelection} beats ${computerSelection}`
+    }
+}
+
 
 function capitalize(word) {
     let firstLetter = word.charAt(0)
@@ -27,10 +42,7 @@ function capitalize(word) {
 }
 
 
+
 // Test your outputs below to ensure correct values
-temp = capitalize("PAPEr")
+temp = playRound("PAPEr")
 console.log(temp)
-
-
-
-
